c14e1c7062017f92d04b930916da0c9040372a1e
Sanjay <sanjaygeorge16@gmail.com>
2017-12-05 12:34:57 +0530

    Add download image code

--- app/src/main/java/com/example/divyaje/voice/showGif.java
+++ app/src/main/java/com/example/divyaje/voice/showGif.java
@@ -1,10 +1,21 @@
 package com.example.divyaje.voice;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.VideoView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 
 public class showGif extends AppCompatActivity {
@@ -20,7 +31,7 @@ public class showGif extends AppCompatActivity {
         Log.e("Test message : ", "This works");
 
         TextView text1 = (TextView) findViewById(R.id.textView);
-        VideoView video1 = (VideoView) findViewById(R.id.videoView);
+        ImageView image1 = (ImageView) findViewById(R.id.imageView);
 
         String url = getIntent().getStringExtra("url");
         String english = getIntent().getStringExtra("english");
@@ -28,7 +39,44 @@ public class showGif extends AppCompatActivity {
         // DISPLAY
         text1.setText(english);
 
+        // DOWNLOAD IMG
+        ImageDownloader task = new ImageDownloader();
+        Bitmap image;
+
+        try {
+            image = task.execute(url).get();
+            image1.setImageBitmap(image);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
 
     }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream input = connection.getInputStream();
+                Bitmap img = BitmapFactory.decodeStream(input);
+                return img;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
 }
