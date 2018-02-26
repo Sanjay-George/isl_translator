e4afced98f5ff447a2452ccda277d9957d46231a
Aabha Mathews <mathews.abby96@gmail.com>
2018-02-26 23:55:30 +0530

    Add server ip text box

--- app/src/main/java/com/example/divyaje/voice/MainActivity.java
+++ app/src/main/java/com/example/divyaje/voice/MainActivity.java
@@ -146,7 +146,7 @@ public class MainActivity extends Activity {
                                         return view;
                                     }
 
-                                    ;
+                                    
                                 });
 
                     }
