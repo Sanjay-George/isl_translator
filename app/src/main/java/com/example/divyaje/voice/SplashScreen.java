9e2621772805fccad01538a32789100a05e520b8
Sanjay George <sanjaygeorge16@gmail.com>
2018-05-27 13:57:06 +0530

    Fix splash screen icon + Minor UI fixes

--- app/src/main/java/com/example/divyaje/voice/SplashScreen.java
+++ app/src/main/java/com/example/divyaje/voice/SplashScreen.java
@@ -11,7 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
